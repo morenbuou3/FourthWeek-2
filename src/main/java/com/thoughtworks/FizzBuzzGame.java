a2d915a375c4f9e865014d2c4897a73bf48cb782
king <369558172@qq.com>
2018-04-12 16:34:17 +0800

    ADD GETANSWER METHOD

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -2,10 +2,40 @@ package com.thoughtworks;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
+    private Integer fizz;
+    private Integer buzz;
+    private Integer whizz;
+
+    public FizzBuzzGame() {
+        fizz = 3;
+        buzz = 5;
+        whizz = 7;
+    }
+
+    public boolean setNumber(Integer fizz, Integer buzz, Integer whizz) {
+        if (fizz <= 0 || fizz > 9) {
+            return false;
+        }
+        if (buzz <= 0 || buzz > 9) {
+            return false;
+        }
+        if (whizz <= 0 || whizz > 9) {
+            return false;
+        }
+        if (fizz.equals(buzz) || fizz.equals(whizz) || buzz.equals(whizz)) {
+            return false;
+        }
+        this.fizz = fizz;
+        this.buzz = buzz;
+        this.whizz = whizz;
+        return true;
+    }
 
     public void start(int count) {
 
@@ -13,6 +43,28 @@ public class FizzBuzzGame {
 
     public List<String> getResults() {
         return results;
+    }
+
+    public String getAnswer(int i) {
+        if (i % fizz == 0 && i % buzz == 0) {
+            return "FizzBuzz";
+        }
+        if (i % fizz == 0 && i % whizz == 0) {
+            return "FizzWhizz";
+        }
+        if (i % buzz == 0 && i % whizz == 0) {
+            return "BuzzWhizz";
+        }
+        if (i % fizz == 0) {
+            return "Fizz";
+        }
+        if (i % buzz == 0) {
+            return "Buzz";
+        }
+        if (i % whizz == 0) {
+            return "Whizz";
+        }
+        return null;
     }
 }
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,88 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class FizzBuzzGameTest {
+
+    private FizzBuzzGame fizzBuzzGame;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzzGame = new FizzBuzzGame();
+    }
+
+    @Test
+    public void should_return_boolen_when_inputNumber() {
+        assertTrue(fizzBuzzGame.setNumber(1, 2, 3));
+        assertTrue(!fizzBuzzGame.setNumber(0, 2, 3));
+        assertTrue(!fizzBuzzGame.setNumber(-5, 1, 3));
+        assertTrue(!fizzBuzzGame.setNumber(2, 1, 10));
+        assertTrue(!fizzBuzzGame.setNumber(2, 2, 1));
+    }
+
     @Test
     public void should_return_Fizz_when_have_3() {
+        String expect = "Fizz";
+        String actual = fizzBuzzGame.getAnswer(3);
+        assertEquals(expect, actual);
+    }
 
+    @Test
+    public void should_return_Buzz_when_have_5() {
+        String expect = "Buzz";
+        String actual = fizzBuzzGame.getAnswer(5);
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void should_return_Whizz_when_have_7() {
+        String expect = "Whizz";
+        String actual = fizzBuzzGame.getAnswer(7);
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void should_return_Fizz_when_have_3_times() {
+        String expect = "Fizz";
+        String actual = fizzBuzzGame.getAnswer(6);
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void should_return_Buzz_when_have_5_times() {
+        String expect = "Buzz";
+        String actual = fizzBuzzGame.getAnswer(10);
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void should_return_Whizz_when_have_7_times() {
+        String expect = "Whizz";
+        String actual = fizzBuzzGame.getAnswer(14);
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void should_return_FizzBuzz_when_have_15() {
+        String expect = "FizzBuzz";
+        String actual = fizzBuzzGame.getAnswer(15);
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void should_return_FizzWhizz_when_have_21() {
+        String expect = "FizzWhizz";
+        String actual = fizzBuzzGame.getAnswer(21);
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    public void should_return_BuzzWhizz_when_have_35() {
+        String expect = "BuzzWhizz";
+        String actual = fizzBuzzGame.getAnswer(35);
+        assertEquals(expect, actual);
     }
 }
